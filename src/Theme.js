9fe3ef64981bd753998eea324fa868b8987f63ea
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-06-19 13:10:06 -0400

    Start styling the header

--- src/Theme.js
+++ src/Theme.js
@@ -7,8 +7,9 @@ const Theme = {
   fontWeight: '400',
   maxWidth: '1000px',
   mobileWidth: '480px',
-  tfBlue: '#0094ff',
-  white: '#fff',
+  orange: 'hsl(14, 78%, 52%)',
+  tfBlue: 'hsl(205, 100%, 50%)',
+  white: 'hsl(0, 0%, 100%)',
 };
 
 export default Theme;
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,41 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background-color: ${props => props.theme.orange};
+  color: ${props => props.theme.white};
+  height: 100px;
+
+  h1,
+  a,
+  a:active,
+  a:hover,
+  a:focus,
+  a:visited {
+    color: ${props => props.theme.white};
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const HeaderContents = styled.div`
+  display: grid;
+  margin: 0 auto;
+  width: 960px;
+`;
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
+  <StyledHeader>
+    <HeaderContents>
       <h1>
         <Link to="/">{siteTitle}</Link>
       </h1>
-    </div>
-  </header>
+    </HeaderContents>
+  </StyledHeader>
 );
 
 Header.propTypes = {
