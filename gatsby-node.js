73332690f47e26bfac0fbc6c55dbf9211484cbd7
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-09-09 12:18:15 -0400

    Add CSV of final data and generate pages from each CSV row

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,71 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+const _ = require('lodash');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    query theIndex {
+      allIndexCsv {
+        edges {
+          node {
+            ISO_2
+            ISO_3
+            country
+            year
+            corporate_rate
+            loss_carryback
+            loss_carryforward
+            machines_cost_recovery
+            buildings_cost_recovery
+            intangibles_cost_recovery
+            inventory
+            patent_box
+            r_and_d_credit
+            corporate_time
+            profit_payments
+            other_payments
+            top_income_rate
+            threshold_top_income_rate
+            tax_wedge
+            labor_payments
+            labor_time
+            capital_gains_rate
+            index_capital_gains
+            dividends_rate
+            vat_rate
+            vat_threshold
+            vat_base
+            consumption_time
+            property_tax
+            property_tax_collections
+            net_wealth
+            estate_or_inheritance_tax
+            transfer_tax
+            asset_tax
+            capital_duties
+            financial_transaction_tax
+            dividends_exemption
+            capital_gains_exemption
+            country_limitations
+            dividends_withholding_tax
+            interest_withholding_tax
+            royalties_withholding_tax
+            tax_treaties
+            cfc_rules
+            thin_capitalization_rules
+          }
+        }
+      }
+    }
+  `);
+  result.data.allIndexCsv.edges.forEach(({ node }) => {
+    createPage({
+      path: _.kebabCase(node.country),
+      component: path.resolve('./src/templates/country.js'),
+      context: {
+        ISO_3: node.ISO_3,
+      },
+    });
+  });
+};
--- src/templates/country.js
+++ src/templates/country.js
@@ -1,15 +1,72 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
-const country = () => (
-  <Layout>
-    <SEO />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
-);
+const country = ({ data }) => {
+  const theCountry = data.indexCsv;
+
+  return (
+    <Layout>
+      <SEO />
+      <h1>{theCountry.country}</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+    </Layout>
+  );
+};
 
 export default country;
+
+export const query = graphql`
+  query($ISO_3: String!) {
+    indexCsv(ISO_3: { eq: $ISO_3 }) {
+      ISO_2
+      ISO_3
+      country
+      year
+      corporate_rate
+      loss_carryback
+      loss_carryforward
+      machines_cost_recovery
+      buildings_cost_recovery
+      intangibles_cost_recovery
+      inventory
+      patent_box
+      r_and_d_credit
+      corporate_time
+      profit_payments
+      other_payments
+      top_income_rate
+      threshold_top_income_rate
+      tax_wedge
+      labor_payments
+      labor_time
+      capital_gains_rate
+      index_capital_gains
+      dividends_rate
+      vat_rate
+      vat_threshold
+      vat_base
+      consumption_time
+      property_tax
+      property_tax_collections
+      net_wealth
+      estate_or_inheritance_tax
+      transfer_tax
+      asset_tax
+      capital_duties
+      financial_transaction_tax
+      dividends_exemption
+      capital_gains_exemption
+      country_limitations
+      dividends_withholding_tax
+      interest_withholding_tax
+      royalties_withholding_tax
+      tax_treaties
+      cfc_rules
+      thin_capitalization_rules
+    }
+  }
+`;
