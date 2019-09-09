63b90a44e73fd45980330154d7a5d411f9c5100f
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-09-09 10:08:42 -0400

    Begin creating template for countries

---
+++ src/templates/country.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+const country = () => (
+  <Layout>
+    <SEO />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </Layout>
+);
+
+export default country;
