51532831b22096ead2283eaf15ea8634d3d6175b
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-09-10 10:14:09 -0400

    Output rankings table to index page

---
+++ src/data/useIndexRankings.js
@@ -0,0 +1,31 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+export const useIndexRankings = () => {
+  const { allIndexCsv } = useStaticQuery(graphql`
+    query rankings {
+      allIndexCsv {
+        edges {
+          node {
+            country
+            year
+            corporate_rank
+            corporate
+            consumption_rank
+            consumption
+            property_rank
+            property
+            income_rank
+            income
+            international_rank
+            international
+            final_rank
+            final
+            ISO_2
+            ISO_3
+          }
+        }
+      }
+    }
+  `);
+  return allIndexCsv.edges.map(node => node.node);
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,44 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import { useIndexRankings } from '../data/useIndexRankings';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
-);
+const IndexPage = () => {
+  const rankings = useIndexRankings();
+  return (
+    <Layout>
+      <SEO />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Overall Rank</th>
+            <th>Corporate Taxes</th>
+            <th>Individual Taxes</th>
+            <th>Consumption Taxes</th>
+            <th>Property Taxes</th>
+            <th>International Tax Rates</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rankings.map(country => (
+            <tr key={`rankings-table-${country.ISO_3}`}>
+              <td>{country.country}</td>
+              <td>{country.final_rank}</td>
+              <td>{country.corporate_rank}</td>
+              <td>{country.income_rank}</td>
+              <td>{country.consumption_rank}</td>
+              <td>{country.property_rank}</td>
+              <td>{country.international_rank}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
 
 export default IndexPage;
