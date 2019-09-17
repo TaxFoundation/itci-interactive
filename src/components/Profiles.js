2bfb26156e7d6d78372518bd8d81df10a0f81ce6
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-09-17 15:03:27 -0400

    Style country profile information

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { range } from 'lodash';
 
+const ProfilesContainer = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const ProfileHeading = styled.h3`
+  font-size: 1.4em;
+  font-weight: 700;
+`;
+
+const ProfileList = styled.ul`
+  list-style: none;
+  padding-left: 1.4rem;
+
+  li::before {
+    content: '•';
+    color: ${props => (props.positive ? 'green' : 'red')};
+    display: inline-block;
+    font-size: 1.4rem;
+    margin-left: -1.4rem;
+    width: 1.4rem;
+  }
+`;
+
 const Profiles = ({ profiles }) => (
-  <div>
-    <h3>Strengths</h3>
-    <ul>
-      {range(3).map(n => (
-        <li key={`strength-${n + 1}`}>{profiles[`strength_${n + 1}`]}</li>
-      ))}
-    </ul>
-    <h3>Weaknesses</h3>
-    <ul>
-      {range(3).map(n => (
-        <li key={`weakness-${n + 1}`}>{profiles[`weakness_${n + 1}`]}</li>
-      ))}
-    </ul>
-  </div>
+  <ProfilesContainer>
+    <section>
+      <ProfileHeading>Strengths</ProfileHeading>
+      <ProfileList positive>
+        {range(3).map(n => (
+          <li key={`strength-${n + 1}`}>{profiles[`strength_${n + 1}`]}</li>
+        ))}
+      </ProfileList>
+    </section>
+    <section>
+      <ProfileHeading>Weaknesses</ProfileHeading>
+      <ProfileList>
+        {range(3).map(n => (
+          <li key={`weakness-${n + 1}`}>{profiles[`weakness_${n + 1}`]}</li>
+        ))}
+      </ProfileList>
+    </section>
+  </ProfilesContainer>
 );
 
 Profiles.propTypes = {
