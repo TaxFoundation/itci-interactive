065e2679dee4960164132575880b3b995f603c04
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-09-26 11:20:32 -0400

    Add desktop menu component

--- src/components/DesktopMenu.js
+++ src/components/DesktopMenu.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const DesktopMenu = props => <div>stuff</div>;
+const StyledDesktopMenu = styled.div`
+  @media screen and (max-width: 700px) {
+    display: none;
+  }
+`;
+
+const DesktopMenu = props => <StyledDesktopMenu>stuff</StyledDesktopMenu>;
 
 DesktopMenu.propTypes = {};
 
