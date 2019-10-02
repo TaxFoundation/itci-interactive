dac9a0acf63aea6c5320fc858e836ac947c9e22c
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-10-02 09:21:03 -0400

    Add Google Tag Manager for production

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-PR5M9GQ',
+        includeInDevelopment: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
