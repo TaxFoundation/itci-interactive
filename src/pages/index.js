72caf4d23f7141f8c0297b4fcaf949ee6b309f34
andrewksloan <andrewksloan3@gmail.com>
2021-10-18 13:35:26 -0700

    revert text change used to verify netlify build.

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => (
         investment and neutrality through a well-structured tax code. The{' '}
         <em>ITCI</em> considers more than 40 variables across five categories:
         Corporate Taxes, Individual Taxes, Consumption Taxes, Property Taxes,
-        and Crossborder Tax Rules.
+        and Cross-border Tax Rules.
       </p>
       <p>
         The <em>ITCI</em> attempts to display not only which countries provide
